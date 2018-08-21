a86d4fb72d286f962358ea4e8d2181c0298b6721
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-21 20:34:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
     let actualRange = parseInt(endBlock) - parseInt(startBlock)
-    let rangeDifference = actualRange - blockRange
+    let rangeDifference = Math.abs(actualRange - blockRange)
 
     if (actualRange > blockRange) {
       return `${rangeDifference} blocks out of range`
