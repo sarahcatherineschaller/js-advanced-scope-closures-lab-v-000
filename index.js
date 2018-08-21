2f0eec6040b8068ac8bea28632a6ea98f184b899
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-21 20:36:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ function produceTipCalculator(percentage) {
     return fare * percentage
   }
 }
+
+function createDriver() {
+  let driverId = 0 
+  return class {
+    constructor(name) {
+      this.name = name 
+      this.id = ++driverId
+    }
+  }
+}
