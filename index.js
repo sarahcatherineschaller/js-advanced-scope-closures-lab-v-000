44a0ce25089e40c406659102de8c7fb2a5110065
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-21 20:28:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
     let actualRange = parseInt(endBlock) - parseInt(startBlock)
     let rangeDifference = actualRange - blockRange
-
-    if (actualRange > blockRange) {
-      return `${rangeDifference} blocks out of range`
-    } else {
-      return `within range by ${rangeDifference}`
-    }
+    // 
+    // if (actualRange > blockRange) {
+    //   return `${rangeDifference} blocks out of range`
+    // } else {
+    //   return `within range by ${rangeDifference}`
+    // }
     }
   }
 }
